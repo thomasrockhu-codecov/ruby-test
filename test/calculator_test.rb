b0cde36015f8ea064bda0e97bf9c48c1f67e95b9
Tom Hu <tom@codecov.io>
2023-01-08 23:30:17 -0800

    fix: add subtract tests

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -6,4 +6,8 @@ class TestCalculator < Minitest::Test
   def test_add
     assert_equal(3.0, Calculator.add(1, 2))
   end
+
+  def test_add
+    assert_equal(-1.0, Calculator.subtract(1, 2))
+  end
 end
